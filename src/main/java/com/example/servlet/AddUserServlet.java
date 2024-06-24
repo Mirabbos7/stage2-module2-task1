cf07239d576e44097e48342fce9038fbe477c93f
Mirabbos <mirabbosahmedov7777@gmail.com>
2024-06-25 00:50:39 +0500

    Test commit

--- src/main/java/com/example/servlet/AddUserServlet.java
+++ src/main/java/com/example/servlet/AddUserServlet.java
@@ -1,5 +1,22 @@
 package com.example.servlet;
 
+import com.example.User;
+import com.example.Warehouse;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/add")
 public class AddUserServlet {
-    //write your code here!
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp, String firstName, String lastName) throws ServletException, IOException {
+        if(firstName != null && lastName != null){
+            User user = new User(firstName, lastName);
+            Warehouse.getInstance().addUser(user);
+        }
+        req.getRequestDispatcher("webapp/jsp/add.jsp").forward(req, resp);
+    }
+
 }
--- src/main/java/com/example/servlet/GetUsersServlet.java
+++ src/main/java/com/example/servlet/GetUsersServlet.java
@@ -1,5 +1,18 @@
 package com.example.servlet;
 
+import com.example.User;
+import com.example.Warehouse;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Set;
+
+@WebServlet("/users")
 public class GetUsersServlet {
-    //write your code here!
+    protected void doGet(HttpServletResponse response, HttpServletRequest request){
+        Set<User> users = Warehouse.getInstance().getUsers();
+        request.setAttribute("User", users);
+        request.getRequestDispatcher("webapp/jsp/users.jsp");
+    }
 }
